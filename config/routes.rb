5bada0163ff56c0e58d49ed971300c0d5ca0a15d
helciodev <helcioandre@live.com>
2021-11-03 12:05:01 +0100

    Add authorization for user with token

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
     end
   end
   post 'auth/login', to: 'authentication#authenticate'
-
+  post 'signup', to: 'users#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
